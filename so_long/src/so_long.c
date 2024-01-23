280b7642b57be7358bfa0c8ecb2c874f7990342c
albartol <albartol@student.42madrid.com>
2024-01-24 00:26:02 +0100

    Makefile for linux

--- so_long/src/so_long.c
+++ so_long/src/so_long.c
@@ -26,7 +26,8 @@ void	ft_print_array(char **map)
 
 void	ft_leaks(void)
 {
-	system("leaks so_long");
+	int i = system("leaks so_long");
+	(void)i;
 }
 
 int  ft_close(int keycode, t_display *display)
@@ -35,6 +36,35 @@ int  ft_close(int keycode, t_display *display)
 	mlx_destroy_window(display->mlx, display->win);
 	exit(EXIT_SUCCESS);
 	return (0);
+}
+
+void	ft_put_pixel(t_display *display)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < 64)
+	{
+		j = 0;
+		while (j < 64)
+			mlx_pixel_put(display->mlx, display->win, i, j++, 0x00FF0000);
+		i++;
+	}
+	while (i < 128)
+	{
+		j = 0;
+		while (j < 64)
+			mlx_pixel_put(display->mlx, display->win, i, j++, 0x0000FF00);
+		i++;
+	}
+	while (i < 192)
+	{
+		j = 0;
+		while (j < 64)
+			mlx_pixel_put(display->mlx, display->win, i, j++, 0x000000FF);
+		i++;
+	}
 }
 
 int	main(int argc, char **argv)
@@ -48,9 +78,10 @@ int	main(int argc, char **argv)
 	map = ft_get_map(argv[1]);
 	ft_check_map(map);
 	ft_display(map, &display);
+	ft_put_pixel(&display);
 	ft_print_array(map);
 	ft_free_array(map);
-	mlx_hook(display.win, 2, 0, ft_close, &display);
+	mlx_hook(display.win, 2, (1L<<0), ft_close, &display);
 	mlx_loop(display.mlx);
 	return (EXIT_SUCCESS);
 }
